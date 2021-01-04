30e2836244b6be28b8f2dd635b6fd1828c52bcd5
jackyli97 <xwlh808@gmail.com>
2021-01-04 08:59:19 -0800

    Complete queue project

--- queue_project/lib/queue.js
+++ queue_project/lib/queue.js
@@ -21,11 +21,48 @@
 // -----------
 
 class Node {
-
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Queue {
+    constructor(){
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
 
+    size(){
+        return this.length;
+    }
+
+    enqueue(val){
+        const node  = new Node(val);
+        if (!this.front){
+            this.front = node;
+            this.back = node;
+        }else{
+            const oldBack = this.back;
+            oldBack.next = node;
+            this.back = node;
+        }
+        this.length++;
+        return this.size();
+    }
+
+    dequeue(){
+        if (!this.front)return null;
+        const node = this.front;
+
+        if (this.front === this.back) this.back = null;
+        const newFront = this.front.next;
+        this.front = newFront;
+
+        this.length--;
+        return node.value;
+    }
 }
 
 exports.Node = Node;
