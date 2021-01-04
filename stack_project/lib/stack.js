6b5c87f0085d890a0b4cd4e8086ff0f74351afab
jackyli97 <xwlh808@gmail.com>
2021-01-04 08:49:08 -0800

    Complete stack project

--- stack_project/lib/stack.js
+++ stack_project/lib/stack.js
@@ -21,11 +21,52 @@
 // -----------
 
 class Node {
-
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    } 
 
+    size() {
+        return this.length;
+    }
+
+    push(val) {
+        const node = new Node(val);
+
+        if (!this.top){
+            this.top = node;
+            this.bottom = node;
+        }
+        else{
+            const temp = this.top;
+            node.next = temp;
+            this.top = node;
+        }
+        this.length++;
+        return this.size();
+    }
+
+    pop() {
+        if (!this.top) return null;
+        const node = this.top;
+        if (this.top === this.bottom){
+            this.top = null;
+            this.bottom = null;
+            this.length--;
+            return node.value;
+        }
+        this.top = this.top.next;
+        this.length--;
+        return node.value;
+    }
 }
 
 exports.Node = Node;
