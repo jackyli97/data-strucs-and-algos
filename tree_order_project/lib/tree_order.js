bc3bf96a5331def617379a466ca5d41d6f2e4977
jackyli97 <xwlh808@gmail.com>
2021-01-07 08:55:08 -0800

    Complete tree node and tree order

--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -1,9 +1,25 @@
 function inOrderArray(root) {
+    if (!root) return [];
 
+    let output = [];
+
+    output = output.concat(inOrderArray(root.left));
+    output.push(root.val);
+    output = output.concat(inOrderArray(root.right));
+
+    return output;
 }
 
 function postOrderArray(root) {
+    if (!root) return [];
 
+    let output = [];
+
+    output = output.concat(postOrderArray(root.left));
+    output = output.concat(postOrderArray(root.right));
+    output.push(root.val);
+
+    return output;
 }
 
 
